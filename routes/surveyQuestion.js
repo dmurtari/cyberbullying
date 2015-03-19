bc4b0fba8d46582523011d7dee7062fbdc9d11e1
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-03-18 19:48:26 -0600

    Added styling for the comments (still needs work) and two buttons that take you to the previous and next posts

--- routes/surveyQuestion.js
+++ routes/surveyQuestion.js
@@ -1,11 +1,14 @@
 module.exports = function(app) {
-  
+
   var _ = require('lodash')
 
   app.get('/survey/list/:id', function(req, res){
     var post = _.find(app.data.survey, {'id': Number(req.params.id)})
+    var length = app.data.survey.length
+
     res.render('surveyQuestion.jade', {
-      post: post
+      post: post,
+      survey_length: length
     })
   })
 }
