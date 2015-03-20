0fabbe6d208248b26558cac7cd845f8b5fd340d1
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-03-19 19:00:08 -0600

    Added the ability to store to the survey data

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var _ = require('lodash')
 
 app.data = {}
 app.data.survey = require('./data/cyberbullying_data.json').data
-app.data.results = require('./data/survey_results.json')
 app.data.questions = require('./data/survey_questions.json').data
 
 // use jade as the view engine
@@ -21,9 +20,35 @@ app.get('/', function(req, res) {
 })
 
 app.get('/survey/list/:id/:q1?&:q2', function(req, res) {
-    console.log(app.data.results)
-    console.log(req.params.q1)
-    console.log(req.params.q2)
+    var results = require('./data/survey_results.json')
+    var q1 = req.params.q1
+    var q2 = req.params.q2
+    var id = req.params.id
+
+    var post = _.find(results.survey_results, {'id': id})
+    console.log(id)
+    console.log(results.survey_results)
+    console.log(post)
+
+    if (post) {
+        result_data = {
+            '1' : q1,
+            '2' : q2
+        }
+        post['results'].push(result_data)
+
+    } else {
+        var result_data = {}
+        result_data['id'] = id
+        result_data['results'] = [{
+            '1' : q1,
+            '2' : q2
+        }]
+
+        results.survey_results.push(result_data)
+    }
+
+    fs.writeFileSync('./data/survey_results.json', JSON.stringify(results, null, 4));
 })
 
 // load routes for note, account, context
