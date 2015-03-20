b550c619584c42351ce7efff23cc21bc3d3b5317
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-19 19:16:57 -0600

    Submit and previous buttons work

--- public/style/custom.js
+++ public/style/custom.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   var next_button = $("#next-button")
-
-  console.log(next_button[0].dataset.id)
+  var prev_button = $("#prev-button")
+  var submit_button = $("#submit-button")
 
   next_button.on("click", function(event) {
     var question1 = $('input[name=question1]:checked').val()
@@ -11,5 +11,25 @@ $( document ).ready(function() {
     console.log(question2)
 
     window.location = "/survey/list/" + question1 + "&" + question2 + "&" + next_button[0].dataset.id
+  })
+
+  prev_button.on("click", function(event) {
+    var question1 = $('input[name=question1]:checked').val()
+    var question2 = $('input[name=question2]:checked').val()
+
+    console.log(question1)
+    console.log(question2)
+
+    window.location = "/survey/list/" + question1 + "&" + question2 + "&" + prev_button[0].dataset.id
+  })
+
+  submit_button.on("click", function(event) {
+    var question1 = $('input[name=question1]:checked').val()
+    var question2 = $('input[name=question2]:checked').val()
+
+    console.log(question1)
+    console.log(question2)
+
+    window.location = "/survey/list/" + question1 + "&" + question2 + "&" + submit_button[0].dataset.id
   })
 })
