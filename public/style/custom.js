b75c85bc91a9ea38a6b5689c5a86cfac35ad51ba
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-19 19:13:29 -0600

    Buttons form URLs

--- public/style/custom.js
+++ public/style/custom.js
@@ -4,11 +4,12 @@ $( document ).ready(function() {
   console.log(next_button[0].dataset.id)
 
   next_button.on("click", function(event) {
-    var selectedVal = ""
-    var selected = $("#question1 input[type='radio']:checked");
-    if (selected.length > 0) {
-      selectedVal = selected.val()
-    }
-    console.log($('input[name=question1]:checked').val())
+    var question1 = $('input[name=question1]:checked').val()
+    var question2 = $('input[name=question2]:checked').val()
+
+    console.log(question1)
+    console.log(question2)
+
+    window.location = "/survey/list/" + question1 + "&" + question2 + "&" + next_button[0].dataset.id
   })
 })
